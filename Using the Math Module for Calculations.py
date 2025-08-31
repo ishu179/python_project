133c11dcbd049e53fb099426a3ab2ad42b273ed7
ishu179 <116792311+ishu179@users.noreply.github.com>
2025-08-31 20:11:00 +0530

    Add files via upload

---
+++ Using the Math Module for Calculations.py
@@ -0,0 +1,13 @@
+# Using the Math Module for Calculations
+
+import math
+
+num = float(input("Enter a number: "))
+
+square_root = math.sqrt(num)
+logarithm = math.log(num)
+sine_value = math.sin(num)
+
+print(f"Square root: {square_root}")
+print(f"Logarithm: {logarithm}")
+print(f"Sine: {sine_value}")
